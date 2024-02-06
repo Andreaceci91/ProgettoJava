4582a3df124afc87ced13ad171a72bce38f95968
andrea <andrea.ceci091@icloud.com>
2024-02-06 23:48:54 +0100

    Commit di prova

--- src/controller/ControlManager.java
+++ src/controller/ControlManager.java
@@ -5,9 +5,13 @@ import model.MatchManager;
 public class ControlManager {
     public static void main(String[] args){
 
+        System.out.println("ciao");
+
         MatchManager matchManager = new MatchManager(2);
         do {
             matchManager.turnoDiGioco();
         }while(true);
+
+
     }
 }
